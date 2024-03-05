42482800bdb701639b71a8ec04abf429a197feb2
patrick <patrickkuo@me.com>
2024-03-05 14:33:51 -0800

    fix tests

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -12,7 +12,7 @@ use tracing::{info, warn};
 
 /// The minimum and maximum protocol versions supported by this build.
 const MIN_PROTOCOL_VERSION: u64 = 1;
-const MAX_PROTOCOL_VERSION: u64 = 38;
+const MAX_PROTOCOL_VERSION: u64 = 39;
 
 // Record history of protocol version allocations here:
 //
